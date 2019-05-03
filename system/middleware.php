25b27c08a734d3ef39d1bcda2dc292b20752e6da
muratsatir <murat.satir@gmail.com>
2019-05-03 08:24:23 +0300

    geliştirme

--- system/middleware.php
+++ system/middleware.php
@@ -3,25 +3,32 @@ function is_login(){
 	global $db;
 	global $user;
 	if(isset($_SESSION['user_id']) && (int)$_SESSION['user_id']>0){
-		
+
 		$user_id=(int)$_SESSION['user_id'];
-		
+
 		$user_query=$db->prepare("SELECT * FROM user WHERE id=:id");
-		$user_query->bindParam(':id', $user_id, PDO::PARAM_STR);			
-		$user_query->execute();	
+		$user_query->bindParam(':id', $user_id, PDO::PARAM_STR);
+		$user_query->execute();
 		$user_detail=$user_query->fetchObject();
-		
+
 		$user->id=$user_detail->id;
 		$user->username=$user_detail->username;
-		
+
 		$profile_query=$db->prepare("SELECT * FROM user_profile WHERE user_id=:user_id");
-		$profile_query->bindParam(':user_id', $user_id, PDO::PARAM_STR);		
-		$profile_query->setFetchMode(PDO::FETCH_OBJ);
-		$profile_detail=$profile_query->fetchObject();		
-		
+		$profile_query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+		$profile_query->execute();
+		$profile_detail=$profile_query->fetchObject();
+
 		$user->profile = new stdClass();
-		$user->profile->email=(isset($profile_detail->email))?$profile_detail->email:'';		
-		
+		$user->profile->email=(isset($profile_detail->email))?$profile_detail->email:'';
+
+		$wallet_query=$db->prepare("SELECT (SELECT SUM(amount) AS total FROM transactions WHERE to_user_id=:user_id) AS in_amout, (SELECT SUM(amount) AS total FROM transactions WHERE from_user_id=:user_id) AS out_amount FROM transactions LIMIT 1");
+		$wallet_query->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+		$wallet_query->execute();
+		$wallet_query_detail=$wallet_query->fetchObject();
+
+		$user->wallet = new stdClass();
+		$user->wallet->total=(float)$wallet_query_detail->in_amount-(float)$wallet_query_detail->out_amount;
 		return true;
 	}
 	else{
@@ -31,8 +38,8 @@ function is_login(){
 }
 function logout(){
 	global $user;
-	
+
 	unset($_SESSION['user_id']);
-	
+
 	$user=new stdClass();
-}	
+}
---
+++ views/account/wallet.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<div class="col-md-4 offset-md-4 text-center">
+		<h2>My Coin Cüzdanım</h2>
+	</div>
+</div>
